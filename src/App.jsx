6b021aa09d0d6a812d8393b6ebea9391a5796e6f
timcheahyi <timcheah1998@gmail.com>
2023-12-12 20:35:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  AddJob,
+  Admin,
+  AllJobs,
+  DashboardLayout,
+  DeleteJob,
+  EditJob,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Profile,
+  Register,
+  Stats,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
